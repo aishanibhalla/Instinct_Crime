77f1bfe35cd53ee1e83fcd6ea1eebfa8d97c3b6f
Aishani Bhalla <aishani.bhalla0709@gmail.com>
2016-07-25 21:15:14 -0700

    Update UI

--- js/call_to_api.js
+++ js/call_to_api.js
@@ -1,25 +1,40 @@
 var map;
 var timeSlider;
+var time;
 var timeExtent_main;
 require([
   "esri/map", "esri/layers/ArcGISDynamicMapServiceLayer",
   "esri/TimeExtent", "esri/dijit/TimeSlider", "esri/urlUtils",
   "esri/arcgis/utils",
-  "esri/layers/FeatureLayer", "esri/InfoTemplate",
+  "esri/layers/FeatureLayer", "esri/InfoTemplate", "myModules/InfoWindow",
   "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol",
   "esri/renderers/UniqueValueRenderer", "esri/Color",
-  "dojo/_base/array", "dojo/dom", "dojo/domReady!"
+  "dojo/_base/array", "dojo/dom", "dojo/dom-construct","dojo/domReady!"
 ], function(
   Map, ArcGISDynamicMapServiceLayer,
-  TimeExtent, TimeSlider, urlUtils, arcgisUtils, FeatureLayer, InfoTemplate,
+  TimeExtent, TimeSlider, urlUtils, arcgisUtils, FeatureLayer, InfoTemplate, InfoWindow,
         SimpleLineSymbol, SimpleFillSymbol,
         UniqueValueRenderer, Color,
-  arrayUtils, dom
+  arrayUtils, dom, domConstruct
 ) {
+  var infoWindow = new  InfoWindow({
+     domNode: domConstruct.create("div", null, dom.byId("mapDiv"))
+  });
   arcgisUtils.createMap("6f046a9aa0be46f18ad45afcb0c17fe7","mapDiv").then(function(response){
     map = response.map;
     console.log(map);
   });
+  // var search = new Search({
+  //       map: map
+  //     }, "ui-dijit-search");
+  // search.startup();
+
+  //create the custom info window specifying any input options
+  //define the info template that is used to display the popup content.
+      var template = new InfoTemplate();
+      template.setTitle("<b>${neighborho}</b>");
+      // template.setContent("hello");
+      template.setContent(getTextContent);
 var main_data;
   function initSlider(date,data) {
     console.log("inside init slider "+date);
@@ -57,14 +72,35 @@ var main_data;
 
     timeSlider.on("time-extent-change", function(evt) {
 
-      var time = timeSlider.getCurrentTimeExtent().endTime.getHours();
+      time = timeSlider.getCurrentTimeExtent().endTime.getHours();
       console.log(time);
       make_feature_layer(data,time);
     });
 
   }
 
+ function getTextContent(graphic){
+   var district = graphic.attributes.neighborho;
+   console.log("district "+district);
 
+   // console.log(Object.keys(main_data[district][0])[0]);
+   // return Object.keys(main_data[district][0])[0];
+
+   var arr = Object.keys( main_data[district][time] ).map(function ( key ) { return main_data[district][time][key]; });
+   var max = Math.max.apply( null, arr );
+   //console.log(max);
+   keys=Object.keys(main_data[district][time]);
+   console.log(keys);
+   var crimes = Object.keys(main_data[district][time]).sort(function(a,b){return main_data[district][time][b] - main_data[district][time][a]});
+   console.log(crimes);
+   //crime = crimes.toString();
+   string="<ul>"
+   for(var x=0;x<crimes.length;x++){
+     string+="<li>"+crimes[x]+"</li>";
+   }
+   string+="</ul>";
+   return string;
+ }
   function make_renderer(time){
     console.log(main_data);
     console.log(time);
@@ -92,17 +128,17 @@ var main_data;
 
 
     });
-    renderer.addValue("ARSON", new SimpleFillSymbol().setColor(new Color([255, 194, 0, 1])));
-    renderer.addValue("ASSAULT", new SimpleFillSymbol().setColor(new Color([62, 78, 184, 1])));
-    renderer.addValue("BURGLARY", new SimpleFillSymbol().setColor(new Color([122, 85, 71, 1])));
-    renderer.addValue("DISORDERLY CONDUCT", new SimpleFillSymbol().setColor(new Color([71, 176, 75, 1])));
-    renderer.addValue("DRUG/NARCOTIC", new SimpleFillSymbol().setColor(new Color([247, 65, 45, 1])));
+    renderer.addValue("ARSON", new SimpleFillSymbol().setColor(new Color([255, 227, 140, 1])));
+    renderer.addValue("ASSAULT", new SimpleFillSymbol().setColor(new Color([120, 138, 255, 1])));
+    renderer.addValue("BURGLARY", new SimpleFillSymbol().setColor(new Color([127, 52, 37, 1])));
+    renderer.addValue("DISORDERLY CONDUCT", new SimpleFillSymbol().setColor(new Color([151, 231, 154, 1])));
+    renderer.addValue("DRUG/NARCOTIC", new SimpleFillSymbol().setColor(new Color([255, 130, 117, 1])));
     renderer.addValue("JUVENILE CRIMES", new SimpleFillSymbol().setColor(new Color([255, 255, 255, 1])));
-    renderer.addValue("LOITERING", new SimpleFillSymbol().setColor(new Color([0, 188, 214, 1])));
-    renderer.addValue("NON-CRIMINAL", new SimpleFillSymbol().setColor(new Color([215, 0, 253, 1])));
-    renderer.addValue("ROBBERY", new SimpleFillSymbol().setColor(new Color([88, 30, 211, 1])));
-    renderer.addValue("VANDALISM", new SimpleFillSymbol().setColor(new Color([236, 21, 97, 1])));
-    renderer.addValue("VEHICLE THEFT", new SimpleFillSymbol().setColor(new Color([0, 151, 136, 1])));
+    renderer.addValue("LOITERING", new SimpleFillSymbol().setColor(new Color([166, 244, 255, 1])));
+    renderer.addValue("NON-CRIMINAL", new SimpleFillSymbol().setColor(new Color([122, 85, 71, 1])));
+    renderer.addValue("ROBBERY", new SimpleFillSymbol().setColor(new Color([243, 175, 255, 1])));
+    renderer.addValue("VANDALISM", new SimpleFillSymbol().setColor(new Color([255, 136, 178, 1])));
+    renderer.addValue("VEHICLE THEFT", new SimpleFillSymbol().setColor(new Color([56, 205, 190, 1])));
     renderer.addValue("WEAPON LAWS", new SimpleFillSymbol().setColor(new Color([216, 216, 216, 1])));
     //console.log(renderer);
     return renderer;
@@ -121,7 +157,7 @@ var main_data;
             var featureLayer = new FeatureLayer("http://services.arcgis.com/6DIQcwlPy8knb6sg/arcgis/rest/services/Neighborhoods/FeatureServer/0", {
                   mode: FeatureLayer.MODE_SNAPSHOT,
                   outFields: ["neighborho"],
-                  //infoTemplate: infoTemplate
+                  infoTemplate: template
             });
             featureLayer.id="custom_layer";
       //       timeSlider.play();
@@ -140,15 +176,13 @@ var main_data;
       //
       // }
 
-
-
-
-
     }
 
   (
   function($){
   function processForm(e){
+    $('#loader').show();
+    document.getElementsByTagName("BODY")[0].style.opacity = .4;
     console.log("hello");
     date=$('#date').val();
     console.log($('#date').val());
@@ -160,6 +194,8 @@ var main_data;
       dataType:'jsonp',
       success:function(data,textStatus,jQxhr){
       console.log("success");
+      $('#loader').hide();
+      document.getElementsByTagName("BODY")[0].style.opacity = 1;
       //console.log(data);
       initSlider(date,data);
       },
--- js/crimecast.js
+++ js/crimecast.js
@@ -1,64 +1,32 @@
 var map;
 require([
-  "esri/map", "esri/layers/ArcGISDynamicMapServiceLayer",
-  "esri/TimeExtent", "esri/dijit/TimeSlider", "esri/urlUtils",
+  "esri/map",
+  "esri/urlUtils",
   "esri/arcgis/utils",
-  "dojo/_base/array", "dojo/dom", "dojo/domReady!"
+  "esri/dijit/Search",
+  "esri/geometry/Extent",
+  "esri/graphic",
+  "esri/symbols/SimpleMarkerSymbol",
+  "esri/geometry/screenUtils",
+  "dojo/_base/array",
+  "dojo/dom",
+  "dojo/dom-construct",
+  "dojo/query",
+  "dojo/_base/Color",
+  "dojo/domReady!"
 ], function(
-  Map, ArcGISDynamicMapServiceLayer,
-  TimeExtent, TimeSlider, urlUtils, arcgisUtils,
-  arrayUtils, dom
+  Map, urlUtils, arcgisUtils,
+  arrayUtils, Search, Extent, Graphic, SimpleMarkerSymbol, screenUtils, dom, domConstruct, query, Color
 ) {
-  arcgisUtils.createMap("ebef4df1bbd240d7aade012c472b2538","mapDiv").then(function(response){
+  arcgisUtils.createMap("6f046a9aa0be46f18ad45afcb0c17fe7","mapDiv").then(function(response){
     map = response.map;
-    //map.on("layers-add-result", initSlider);
-    initSlider();
+    searchFun();
   });
-
-  //var opLayer = new ArcGISDynamicMapServiceLayer("https://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Petroleum/KSWells/MapServer");
-  //opLayer.setVisibleLayers([0]);
-
-  //apply a definition expression so only some features are shown
-  //var layerDefinitions = [];
-  //layerDefinitions[0] = "FIELD_KID=1000148164";
-  //opLayer.setLayerDefinitions(layerDefinitions);
-
-  //add the gas fields layer to the map
-  //map.addLayers([opLayer]);
-
-
-
-  function initSlider() {
-    console.log("I was called");
-    var timeSlider = new TimeSlider({
-      style: "width: 100%;"
-    }, dom.byId("timeSliderDiv"));
-    map.setTimeSlider(timeSlider);
-
-    var timeExtent = new TimeExtent();
-    timeExtent.startTime = new Date("1/1/1921 UTC");
-    timeExtent.endTime = new Date("12/31/2009 UTC");
-    timeSlider.setThumbCount(1);
-    timeSlider.createTimeStopsByTimeInterval(timeExtent, 2, "esriTimeUnitsYears");
-    timeSlider.setThumbIndexes([0,1]);
-    timeSlider.setThumbMovingRate(2000);
-    timeSlider.startup();
-
-    //add labels for every other time stop
-    var labels = arrayUtils.map(timeSlider.timeStops, function(timeStop, i) {
-      if ( i % 2 === 0 ) {
-        return timeStop.getUTCFullYear();
-      } else {
-        return "";
-      }
-    });
-
-    timeSlider.setLabels(labels);
-
-    timeSlider.on("time-extent-change", function(evt) {
-      var startValString = evt.startTime.getUTCFullYear();
-      var endValString = evt.endTime.getUTCFullYear();
-      dom.byId("daterange").innerHTML = "<i>" + startValString + " and " + endValString  + "<\/i>";
-    });
+  function searchFun(){
+    var search = new Search({
+       map: map
+    }, "search");
+    console.log(map);
+    search.startup();
   }
 });
